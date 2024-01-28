12fe39733dfa7b5a10c8eb90e551b18c5c83610b
jbreffle <78390184+jbreffle@users.noreply.github.com>
2024-01-28 17:48:40 -0500

    Set up cookiecutter template

---
+++ hms-eeg-kaggle/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='HMS - Harmful Brain Activity Classification, Kaggle competition',
+    author='jbreffle',
+    license='MIT',
+)
